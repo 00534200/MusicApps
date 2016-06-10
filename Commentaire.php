412b48fd41af3c3d19548c1491b5be14adaf1eaa
nicooooo <nicooooo@outlook.fr>
2016-06-10 16:11:58 -0400

    debut commentaire

---
+++ Commentaire.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Commentaire</title>
+    </head>
+
+    <body>
+    
+      <form method="post" action="">
+   <p>
+       <label for="ameliorer">
+Que pensez vous de cette music?
+       </label>
+       <br />
+       
+       <textarea  id="com" rows="10" cols="50">
+       </textarea>       
+   </p>
+</form>
+      
+      
+    </body>
+</html>
